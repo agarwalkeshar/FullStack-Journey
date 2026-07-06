576bc92ab58e6ea89cc201c30c8cd7e44cc59226
Keshar Agarwal <139530266+agarwalkeshar@users.noreply.github.com>
2026-07-06 23:59:58 +0530

    Day 26 completed

--- Web-Dev/JavaScript Conditionals(Day 26).js
+++ Web-Dev/JavaScript Conditionals(Day 26).js
@@ -1,1 +1,38 @@
 console.log("Hello I am conditional tutorial");
+
+let age = 18;
+let grace = 2;
+
+// age+=grace
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+
+if(age == 18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
+// Comparison Operator
+
+// == equal to sign. used to check if the given value is same as the mentioned variable.
+// != not equal to sign. used to check if the given value is not same as the mentioned variable.
+// === equal value and type sign. It is used to check if both the value and type of the variable is same.
+// !== not equal value or not type sign. It is used to check if both the value and type of the variable are not same.
+// > greater than sign. Used to compare or chefck if the value if greater or not. 
+// < less than sign. Used to compare or check if the value if less or not. 
+// >= greater than or equal to sign. Used to compare or check if the value if greater than or equal to the mentioned variable. 
+// <= less than or equal to sign. Used to compare or check if the value if less than or equal to the mentioned variable.
+// ? ternary operator. 
+
+// Logical Operator
+
+// && = Logical AND. Both the comditions should be "true" for the answer to be true.
+// || = Logical OR. Any 1  conditions can be "true" for the answer to be true.
+// ! = Logical NOT. Opposite
